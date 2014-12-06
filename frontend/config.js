557584c61031e5613affc89c1c22b142d9452bc3
Stewart Atkins <01satkins@gmail.com>
2014-12-06 16:19:53 +0000

    Updated config.js style and removed extra deps from package.json

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,8 @@
-var self
-module.exports = exports = self = {};
-self.webserver = {
-	port: process.env.PORT || 8000
+"use strict";
+
+var self;
+module.exports = exports = self = {
+	"webserver": {
+		"port": process.env.PORT || 8000
+	}
 };
