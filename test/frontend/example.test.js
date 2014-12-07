916baa66373b07299d3e9307dfa9945283fc88b0
Robert Main <lenswipe@gmail.com>
2014-12-07 22:20:16 +0000

    Forgot some files

---
+++ test/frontend/example.test.js
@@ -0,0 +1,20 @@
+describe('MainCtrl', function() {
+  beforeEach(module('app'));
+
+  var $controller;
+
+  beforeEach(inject(function(_$controller_){
+    // The injector unwraps the underscores (_) from around the parameter names when matching
+    $controller = _$controller_;
+  }));
+
+  describe('$scope.grade', function() {
+    it('Gets the value of an example variable', function() {
+      var $scope = {};
+      var controller = $controller('MainCtrl', { $scope: $scope });
+      $scope.password = 'longerthaneightchars';
+      $scope.grade();
+      expect($scope.strength).toEqual('strong');
+    });
+  });
+});
---
+++ test/frontend/karma.conf.js
@@ -0,0 +1,55 @@
+module.exports = function(config) {
+  config.set({
+
+    // base path used to resolve all patterns (e.g. files, exclude)
+    basePath: '../../',
+
+    // frameworks to use
+    frameworks: ['jasmine'],
+
+    // list of files / patterns to load in the browser
+	/*
+		files: [
+	      'bower_components/angular/angular.js',
+	      'bower_components/angular-mocks/angular-mocks.js',
+	      'src/*.js',
+	      'test/*.mocha.js'
+	    ],
+    */
+
+    // list of files to exclude
+    exclude: [],
+
+    // preprocess matching files before serving them to the browser
+/*    preprocessors: {
+      'src/*.js': ['coverage']
+    },*/
+
+    coverageReporter: {
+      type: 'text-summary',
+      dir: 'coverage/'
+    },
+
+    // test results reporter to use
+    reporters: ['progress', 'coverage'],
+
+    // web server port
+    port: 9876,
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+    // level of logging
+    logLevel: config.LOG_INFO,
+
+    // enable / disable watching file and executing tests on file changes
+    autoWatch: true,
+
+    // start these browsers
+    browsers: ['PhantomJS'],
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false
+  });
+};
