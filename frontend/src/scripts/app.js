a96c6c20462b08cdd5541b69aebdb42d7853b93b
Robert Main <lenswipe@gmail.com>
2015-02-13 00:02:44 +0000

    Begun to implement material design

--- frontend/src/scripts/app.js
+++ frontend/src/scripts/app.js
@@ -1,28 +1,31 @@
 (function(){
-	'use strict';
+	"use strict";
 
-	angular.module('pillars', [
-		'pillars.controllers',
-		'pillars.filters',
-		'ui.router'
+	angular.module("pillars", [
+		"pillars.controllers",
+		"pillars.filters",
+		"ui.router",
+		"ngMaterial"
 	])
 
-	.config(['$stateProvider', '$urlRouterProvider', '$httpProvider', '$provide', '$locationProvider', function($stateProvider, $urlRouterProvider, $httpProvider, $provide, $locationProvider){
+	.config(["$stateProvider", "$urlRouterProvider", "$httpProvider", "$provide", "$locationProvider", "$mdThemingProvider", function($stateProvider, $urlRouterProvider, $httpProvider, $provide, $locationProvider, $mdThemingProvider){
+		$mdThemingProvider.theme("default")
+			.primaryPalette('blue');
 		$stateProvider
-			.state('index', {
-				controller: 'MainCtrl',
-				templateUrl: 'views/home.html',
-				url: '/'
+			.state("index", {
+				controller: "MainCtrl",
+				templateUrl: "views/home.html",
+				url: "/"
 			})
 			//Your Custom Routes Go Here
-			.state('404', {
-				templateUrl: 'views/404.html',
-				url: '/404'
+			.state("404", {
+				templateUrl: "views/404.html",
+				url: "/404"
 			});
-		$urlRouterProvider.when('','/').otherwise("/404"); //404 Route
+		$urlRouterProvider.when("","/").otherwise("/404"); //404 Route
 	}])
 
-	.run(['$state', '$rootScope', '$location', function($state, $rootScope, $location){
-		$state.go('index'); //Display the index page on start (redirect to the "app" state)
+	.run(["$state", "$rootScope", "$location", function($state, $rootScope, $location){
+		$state.go("index"); //Display the index page on start (redirect to the "app" state)
 	}]);
 })();
